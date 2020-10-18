7beea7d377da843b79ed3b9846d522082b35df53
mukarramali <mukarram.ali89@gmail.com>
2020-10-18 12:55:29 +0000

    Deploying to gh-pages from  @ aa47791b1d4257527016ccb2afac9e6859895c68 🚀

---
+++ precache-manifest.5fd919e8ce69551a3c7ea1b0d4ea7bed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1469108a81db58d1f1b987c222da5875",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76d65a01f7f256e148ae",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fd0d47dec8fa656bb8b",
+    "url": "./static/js/2.473f647d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.473f647d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d65a01f7f256e148ae",
+    "url": "./static/js/main.01a30e5e.chunk.js"
+  },
+  {
+    "revision": "5ce942ad912b651247bc",
+    "url": "./static/js/runtime-main.a4eb3d90.js"
+  }
+]);
