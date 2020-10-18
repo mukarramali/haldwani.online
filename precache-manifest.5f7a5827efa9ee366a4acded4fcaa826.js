d3299ddbdd29e0f34c6e9db3526cfaf05bb90f37
mukarramali <mukarram.ali89@gmail.com>
2020-10-18 12:45:13 +0000

    Deploying to gh-pages from  @ 57631d632a95ed9fcf660200e059f0d9e8af1956 🚀

---
+++ precache-manifest.5f7a5827efa9ee366a4acded4fcaa826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ed1be4da7d4cad060973ad57cda2c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729acc79d9411e78413a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6fd0d47dec8fa656bb8b",
+    "url": "/static/js/2.473f647d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.473f647d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729acc79d9411e78413a",
+    "url": "/static/js/main.59464c2b.chunk.js"
+  },
+  {
+    "revision": "681adf89645edb311958",
+    "url": "/static/js/runtime-main.7cac42ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mukarramali/haldwani.online/precache-manifest.04d5439b160b1cc55739737cc16c3a1a.js"
+  "/precache-manifest.5f7a5827efa9ee366a4acded4fcaa826.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mukarramali/haldwani.online/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
