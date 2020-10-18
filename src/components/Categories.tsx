786c43fc96d41697131e5ce28aa7e72d3a201c3d
Mukku <mukarram.ali89@gmail.com>
2020-10-18 17:31:13 +0200

    feat: adds customization in categories card

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -4,6 +4,7 @@ import {
   makeStyles,
   Paper,
   Theme,
+  Typography,
 } from "@material-ui/core";
 import React from "react";
 
@@ -11,29 +12,55 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {},
     paper: {
-      height: 140,
-      width: 280,
-      margin: "-50px 10px 10px 10px",
+      padding: 5,
+      margin: "-45px 10px 10px 10px",
     },
   })
 );
 
+interface Category {
+  icon?: string;
+  gradientBackground?: string;
+  title: string;
+  subtitle: string;
+  link?: string;
+}
+
 const Category = (props: any) => {
   const classes = useStyles();
   return (
-    <Grid item>
-      <Paper className={classes.paper} elevation={4} />
+    <Grid item xs={3}>
+      <Paper className={classes.paper} elevation={4}>
+        <Grid container direction="row">
+          <Grid item xs={3}>
+            <img src={props.icon} />
+          </Grid>
+          <Grid item xs={9}>
+            <Typography variant="h5">{props.title}</Typography>
+            <Typography variant="subtitle2">{props.subtitle}</Typography>
+          </Grid>
+        </Grid>
+      </Paper>
     </Grid>
   );
 };
+
+const categoryData: Category[] = [
+  {
+    gradientBackground: "linear-gradient(45deg, #bfc932 30%, #f4fc66 90%)",
+    icon: require("../images/groceries.png"),
+    title: "Groceries",
+    subtitle: "Freshness home delivered",
+  },
+];
 
 export default function Categories() {
   const classes = useStyles();
 
   return (
     <Grid container justify="space-evenly" className={classes.root}>
-      {[1, 2, 3, 4].map((category) => (
-        <Category />
+      {[0, 1, 2, 3].map((category) => (
+        <Category {...categoryData[category]} />
       ))}
     </Grid>
   );
