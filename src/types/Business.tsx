73406508110604774f76d9cb471db84954987cc8
Mukku <mukarram.ali89@gmail.com>
2020-10-18 20:11:38 +0200

    feat: adds business data and type

---
+++ src/types/Business.tsx
@@ -0,0 +1,9 @@
+export default interface Business {
+  title: string;
+  description?: string;
+  icon?: string;
+  link?: string;
+  storePhoto?: string;
+  location?: string;
+  tags?: string[];
+}
---
+++ src/utils/businessesData.tsx
@@ -0,0 +1,13 @@
+import Business from "../types/Business";
+
+const businessesData: Business[] = [
+  {
+    title: "Dusky Lory",
+    description: "Interior Decor Accessories",
+    link: "https://duskylory.store/",
+    storePhoto:
+      "https://cdn.shopify.com/s/files/1/0294/3775/5497/files/int4_900x.jpg?v=1596310157",
+    location: "https://g.page/duskylory?share",
+    tags: ["shopping", "decor"],
+  },
+];
